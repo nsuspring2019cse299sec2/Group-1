6490ff4617c28aee2f2bbc15ad883a1cbe06c4b5
sakibmahmud01 <47084775+sakibmahmud01@users.noreply.github.com>
2019-03-21 19:23:23 +0600

    BottomNavigation
    -buttonsAdded
    -animationFixed

--- ProjectCode/app/src/main/java/com/example/sakibmahmud/project_code/HomeActivity.java
+++ ProjectCode/app/src/main/java/com/example/sakibmahmud/project_code/HomeActivity.java
@@ -2,12 +2,33 @@ package com.example.sakibmahmud.project_code;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.sakibmahmud.project_code.Utils.BottomNavigationViewHelper;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private static final String TAG = "HomeActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Log.d(TAG, "onCreate: starting.");
+
+        setupBottomNavigationView();
     }
+
+
+    /**
+     * Bottom Navigation Setup
+     */
+    private void setupBottomNavigationView(){
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx)findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+        
+    }
+
 }
