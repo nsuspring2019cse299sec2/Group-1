6a667feba93347cea562bfd62cf006a52780aaec
sakibmahmud01 <47084775+sakibmahmud01@users.noreply.github.com>
2019-03-25 23:06:53 +0600

    Profile Toolbar and Menu

--- ProjectCode/app/src/main/java/com/example/sakibmahmud/project_code/Profile/ProfileActivity.java
+++ ProjectCode/app/src/main/java/com/example/sakibmahmud/project_code/Profile/ProfileActivity.java
@@ -1,12 +1,16 @@
 package com.example.sakibmahmud.project_code.Profile;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
 
 import com.example.sakibmahmud.project_code.R;
 import com.example.sakibmahmud.project_code.Utils.BottomNavigationViewHelper;
@@ -20,14 +24,39 @@ public class ProfileActivity extends AppCompatActivity {
     private  static final int ACTIVITY_NUM=4;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_profile);
         Log.d(TAG, "onCreate: started.");
 
-        setupBottomNavigationView();
+        //setupBottomNavigationView();
+        setupToolbar();
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void setupToolbar(){
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.profileToolBar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Log.d(TAG, "onMenuItemClick: clicked");
+                
+                switch (item.getItemId()){
+                    case R.id.profileMenu:
+                        Log.d(TAG, "onMenuItemClick:  Navigatiing to Profile Preference");
+                }
+                
+                return false;
+            }
+        });
+
+    }
+
 
 
     /**
@@ -42,5 +71,11 @@ public class ProfileActivity extends AppCompatActivity {
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return true;
     }
 }
