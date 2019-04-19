09d60b65d02d891fda0038fbfc92330e0c03bc0a
Sakib Mahmud <47084775+sakibmahmud01@users.noreply.github.com>
2019-04-20 01:43:23 +0600

    Profile Fragment
    -Profile Fragment (UI and Dev)
    -setting Profile fragment widget
    -nav edit profile
    -loading profile image from database

--- ProjectCode/app/src/main/java/com/example/sakibmahmud/project_code/Profile/ProfileActivity.java
+++ ProjectCode/app/src/main/java/com/example/sakibmahmud/project_code/Profile/ProfileActivity.java
@@ -6,6 +6,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -43,77 +44,88 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
         Log.d(TAG, "onCreate: started.");
 
+        init();
 
-        setupBottomNavigationView();
-        setupToolbar();
-        setupActivityWidgets();
-        setProfileImage();
-
-        tempGridSetup();
+//        setupBottomNavigationView();
+//        setupToolbar();
+//        setupActivityWidgets();
+//        setProfileImage();
+//
+//        tempGridSetup();
     }
+//
+        private void init(){
+            Log.d(TAG, "init: inflating" + getString(R.string.profile_fragment));
 
-    private void tempGridSetup(){
-        ArrayList<String> imgURLs = new ArrayList<>();
-        imgURLs.add("https://pbs.twimg.com/profile_images/616076655547682816/6gMRtQyY.jpg");
-        imgURLs.add("https://i.redd.it/9bf67ygj710z.jpg");
-        imgURLs.add("https://c1.staticflickr.com/5/4276/34102458063_7be616b993_o.jpg");
-        imgURLs.add("http://i.imgur.com/EwZRpvQ.jpg");
-        imgURLs.add("http://i.imgur.com/JTb2pXP.jpg");
-        imgURLs.add("https://i.redd.it/59kjlxxf720z.jpg");
-        imgURLs.add("https://i.redd.it/pwduhknig00z.jpg");
-        imgURLs.add("https://i.redd.it/clusqsm4oxzy.jpg");
-        imgURLs.add("https://i.redd.it/svqvn7xs420z.jpg");
-        imgURLs.add("http://i.imgur.com/j4AfH6P.jpg");
-        imgURLs.add("https://i.redd.it/89cjkojkl10z.jpg");
-        imgURLs.add("https://i.redd.it/aw7pv8jq4zzy.jpg");
+            ProfileFragment fragment = new ProfileFragment();
+            FragmentTransaction transaction = ProfileActivity.this.getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.container, fragment);
+            transaction.addToBackStack(getString(R.string.profile_fragment));
+            transaction.commit();
+        }
 
-        setupImageGrid(imgURLs);
-    }
-
-    private void setupImageGrid(ArrayList<String> imgURLs){
-        GridView gridView = (GridView) findViewById(R.id.gridView);
-
-        int gridWidth = getResources().getDisplayMetrics().widthPixels;
-        int imageWidth = gridWidth/NUM_GRID_COLUMNS;
-        gridView.setColumnWidth(imageWidth);
-
-        GridImageAdapter adapter = new GridImageAdapter(mContext, R.layout.layout_grid_imageview, "", imgURLs);
-        gridView.setAdapter(adapter);
-    }
-
-
-    private  void setProfileImage(){
-        Log.d(TAG, "setProfileImage: setting profile photo");
-        String imgURL = "https://scontent.fdac18-1.fna.fbcdn.net/v/t1.0-9/52333457_2399136860109931_3149409594426523648_n.jpg?_nc_cat=102&_nc_ht=scontent.fdac18-1.fna&oh=5f18efaacad82c519894278b082b1da9&oe=5D43B4FE";
-        UniversalImageLoader.setImage(imgURL, profilePhoto, mProgressBar, "");
-    }
-
-    private void setupActivityWidgets(){
-        mProgressBar = (ProgressBar) findViewById(R.id.profileProgressBar);
-        mProgressBar.setVisibility(View.GONE);
-        profilePhoto = (ImageView) findViewById(R.id.profile_photo);
-    }
-
-    /**
-     * Responsible for setting up the profile toolbar
-     */
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    private void setupToolbar(){
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.profileToolBar);
-        setSupportActionBar(toolbar);
-
-        ImageView profileMenu = (ImageView) findViewById(R.id.profileMenu);
-        profileMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d(TAG, "onClick: navigating to account settings.");
-                Intent intent = new Intent(mContext, AccountSettingsActivity.class);
-                startActivity(intent);
-            }
-        });
-
-    }
+//    private void tempGridSetup(){
+//        ArrayList<String> imgURLs = new ArrayList<>();
+//        imgURLs.add("https://pbs.twimg.com/profile_images/616076655547682816/6gMRtQyY.jpg");
+//        imgURLs.add("https://i.redd.it/9bf67ygj710z.jpg");
+//        imgURLs.add("https://c1.staticflickr.com/5/4276/34102458063_7be616b993_o.jpg");
+//        imgURLs.add("http://i.imgur.com/EwZRpvQ.jpg");
+//        imgURLs.add("http://i.imgur.com/JTb2pXP.jpg");
+//        imgURLs.add("https://i.redd.it/59kjlxxf720z.jpg");
+//        imgURLs.add("https://i.redd.it/pwduhknig00z.jpg");
+//        imgURLs.add("https://i.redd.it/clusqsm4oxzy.jpg");
+//        imgURLs.add("https://i.redd.it/svqvn7xs420z.jpg");
+//        imgURLs.add("http://i.imgur.com/j4AfH6P.jpg");
+//        imgURLs.add("https://i.redd.it/89cjkojkl10z.jpg");
+//        imgURLs.add("https://i.redd.it/aw7pv8jq4zzy.jpg");
+//
+//        setupImageGrid(imgURLs);
+//    }
+//
+//    private void setupImageGrid(ArrayList<String> imgURLs){
+//        GridView gridView = (GridView) findViewById(R.id.gridView);
+//
+//        int gridWidth = getResources().getDisplayMetrics().widthPixels;
+//        int imageWidth = gridWidth/NUM_GRID_COLUMNS;
+//        gridView.setColumnWidth(imageWidth);
+//
+//        GridImageAdapter adapter = new GridImageAdapter(mContext, R.layout.layout_grid_imageview, "", imgURLs);
+//        gridView.setAdapter(adapter);
+//    }
+//
+//
+//    private  void setProfileImage(){
+//        Log.d(TAG, "setProfileImage: setting profile photo");
+//        String imgURL = "https://scontent.fdac18-1.fna.fbcdn.net/v/t1.0-9/52333457_2399136860109931_3149409594426523648_n.jpg?_nc_cat=102&_nc_ht=scontent.fdac18-1.fna&oh=5f18efaacad82c519894278b082b1da9&oe=5D43B4FE";
+//        UniversalImageLoader.setImage(imgURL, profilePhoto, mProgressBar, "");
+//    }
+//
+//    private void setupActivityWidgets(){
+//        mProgressBar = (ProgressBar) findViewById(R.id.profileProgressBar);
+//        mProgressBar.setVisibility(View.GONE);
+//        profilePhoto = (ImageView) findViewById(R.id.profile_photo);
+//    }
+//
+//    /**
+//     * Responsible for setting up the profile toolbar
+//     */
+//    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+//    private void setupToolbar(){
+//
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.profileToolBar);
+//        setSupportActionBar(toolbar);
+//
+//        ImageView profileMenu = (ImageView) findViewById(R.id.profileMenu);
+//        profileMenu.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d(TAG, "onClick: navigating to account settings.");
+//                Intent intent = new Intent(mContext, AccountSettingsActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//    }
 
 
 
--- ProjectCode/app/src/main/java/com/example/sakibmahmud/project_code/Utils/FirebaseMethods.java
+++ ProjectCode/app/src/main/java/com/example/sakibmahmud/project_code/Utils/FirebaseMethods.java
@@ -7,6 +7,7 @@ import android.widget.Toast;
 
 import com.example.sakibmahmud.project_code.Models.User;
 import com.example.sakibmahmud.project_code.Models.UserAccountSettings;
+import com.example.sakibmahmud.project_code.Models.UserSettings;
 import com.example.sakibmahmud.project_code.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -135,13 +136,114 @@ public class FirebaseMethods {
                 0,
                 0,
                 profile_photo,
-                username,
+                StringManipulation.condenseUsername(username),
                 website
         );
 
         myRef.child(mContext.getString(R.string.dbname_user_account_settings))
                 .child(userID)
                 .setValue(settings);
+
+    }
+
+    /**
+     * Retrieves the account settings for teh user currently logged in
+     * Database: user_acount_Settings node
+     * @param dataSnapshot
+     * @return
+     */
+    public UserSettings getUserSettings(DataSnapshot dataSnapshot){
+        Log.d(TAG, "getUserAccountSettings: retrieving user account settings from firebase.");
+
+
+        UserAccountSettings settings  = new UserAccountSettings();
+        User user = new User();
+
+        for(DataSnapshot ds: dataSnapshot.getChildren()){
+
+            // user_account_settings node
+            if(ds.getKey().equals(mContext.getString(R.string.dbname_user_account_settings))){
+                Log.d(TAG, "getUserAccountSettings: datasnapshot: " + ds);
+
+                try{
+
+                    settings.setDisplay_name(
+                            ds.child(userID)
+                                    .getValue(UserAccountSettings.class)
+                                    .getDisplay_name()
+                    );
+                    settings.setUsername(
+                            ds.child(userID)
+                                    .getValue(UserAccountSettings.class)
+                                    .getUsername()
+                    );
+                    settings.setWebsite(
+                            ds.child(userID)
+                                    .getValue(UserAccountSettings.class)
+                                    .getWebsite()
+                    );
+                    settings.setDescription(
+                            ds.child(userID)
+                                    .getValue(UserAccountSettings.class)
+                                    .getDescription()
+                    );
+                    settings.setProfile_photo(
+                            ds.child(userID)
+                                    .getValue(UserAccountSettings.class)
+                                    .getProfile_photo()
+                    );
+                    settings.setPosts(
+                            ds.child(userID)
+                                    .getValue(UserAccountSettings.class)
+                                    .getPosts()
+                    );
+                    settings.setFollowing(
+                            ds.child(userID)
+                                    .getValue(UserAccountSettings.class)
+                                    .getFollowing()
+                    );
+                    settings.setFollowers(
+                            ds.child(userID)
+                                    .getValue(UserAccountSettings.class)
+                                    .getFollowers()
+                    );
+
+                    Log.d(TAG, "getUserAccountSettings: retrieved user_account_settings information: " + settings.toString());
+                }catch (NullPointerException e){
+                    Log.e(TAG, "getUserAccountSettings: NullPointerException: " + e.getMessage() );
+                }
+
+
+                // users node
+                if(ds.getKey().equals(mContext.getString(R.string.dbname_users))) {
+                    Log.d(TAG, "getUserAccountSettings: datasnapshot: " + ds);
+
+                    user.setUsername(
+                            ds.child(userID)
+                                    .getValue(User.class)
+                                    .getUsername()
+                    );
+                    user.setEmail(
+                            ds.child(userID)
+                                    .getValue(User.class)
+                                    .getEmail()
+                    );
+                    user.setPhone_number(
+                            ds.child(userID)
+                                    .getValue(User.class)
+                                    .getPhone_number()
+                    );
+                    user.setUser_id(
+                            ds.child(userID)
+                                    .getValue(User.class)
+                                    .getUser_id()
+                    );
+
+                    Log.d(TAG, "getUserAccountSettings: retrieved users information: " + user.toString());
+                }
+            }
+        }
+        return new UserSettings(user, settings);
 
     }
 
