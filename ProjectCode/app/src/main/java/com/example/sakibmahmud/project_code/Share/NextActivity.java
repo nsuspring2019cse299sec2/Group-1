a94fbc52a3c7ff41ca806d54133a77dd95d9b1c3
Sakib Mahmud <47084775+sakibmahmud01@users.noreply.github.com>
2019-04-20 20:12:25 +0600

    Upload photo to firebase
    -upload image to firebase
    -getting the image count
    -firebase storage reference
    -convert bitmap to byte array
    -insert photo to firebase database

--- ProjectCode/app/src/main/java/com/example/sakibmahmud/project_code/Share/NextActivity.java
+++ ProjectCode/app/src/main/java/com/example/sakibmahmud/project_code/Share/NextActivity.java
@@ -7,8 +7,10 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.sakibmahmud.project_code.R;
 import com.example.sakibmahmud.project_code.Utils.FirebaseMethods;
@@ -31,15 +33,20 @@ public class NextActivity extends AppCompatActivity {
     private FirebaseDatabase mFirebaseDatabase;
     private DatabaseReference myRef;
     private FirebaseMethods mFirebaseMethods;
+    //widgets
+    private EditText mCaption;
 
     //vars
     private String mAppend = "file:/";
-
+    private int imageCount = 0;
+    private String imgUrl;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+        mFirebaseMethods = new FirebaseMethods(NextActivity.this);
+        mCaption = (EditText) findViewById(R.id.caption) ;
 
         setupFirebaseAuth();
 
@@ -59,19 +66,42 @@ public class NextActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Log.d(TAG, "onClick: navigating to the final share screen.");
                 //upload the image to firebase
+                Toast.makeText(NextActivity.this, "Attempting to upload new photo", Toast.LENGTH_SHORT).show();
+                String caption = mCaption.getText().toString();
+                mFirebaseMethods.uploadNewPhoto(getString(R.string.new_photo), caption, imageCount, imgUrl);
             }
         });
 
         setImage();
     }
+    private void someMethod(){
+        /*
+           Step 1)
+           Created a data model for photos
+
+           Step 2)
+           Add properties to the photo objects (caption, date, imageUrl, photo_id, tags, user_id)
+
+           Step 3)
+           Count the number of photos that the user already has
+
+           Step 4)
+           a) Upload the photo to Firebase Storage
+           b) inter into 'photos' node
+           c) insert into 'user_photos' node
+
+         */
+
+    }
 
     /**
      * gets the image url from the incoming intent and displays the chosen image
      */
-    private void setImage(){
+    private void setImage() {
         Intent intent = getIntent();
         ImageView image = (ImageView) findViewById(R.id.imageShare);
-        UniversalImageLoader.setImage(intent.getStringExtra(getString(R.string.selected_image)), image, null, mAppend);
+        imgUrl = intent.getStringExtra(getString(R.string.selected_image));
+        UniversalImageLoader.setImage(imgUrl, image, null, mAppend);
     }
 
      /*
@@ -86,6 +116,7 @@ public class NextActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         myRef = mFirebaseDatabase.getReference();
+        Log.d(TAG, "onDataChange: image count: " + imageCount);
 
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -108,7 +139,8 @@ public class NextActivity extends AppCompatActivity {
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-
+                imageCount = mFirebaseMethods.getImageCount(dataSnapshot);
+                Log.d(TAG, "onDataChange: image count: " + imageCount);
 
             }
 
